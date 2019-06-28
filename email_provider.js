d93aacde83fb1737ba5640f7b27167d0b994ae4d
tbcodes <soypacos@hotmail.com>
2019-06-28 21:33:32 +0200

    Initial information added.

--- email_provider.js
+++ email_provider.js
@@ -1,7 +1,10 @@
+// Mini Project in Javascript: How to extract email provider name using Javascript code.
+// Javascript code presented in this video: https://www.youtube.com/watch?v=j9ahBGcOyBo
+
 function findEmailProvider(address) {
   const lastIndex = address.lastIndexOf('@');
   return address.slice(lastIndex + 1, address.length);
 }
 
-console.log(findEmailProvider('hellothere@yahoo.com');
-console.log(findEmailProvider('truzzblogg@gmail.com');
+console.log(findEmailProvider('hellothere@yahoo.com'));
+console.log(findEmailProvider('truzzblogg@gmail.com'));
